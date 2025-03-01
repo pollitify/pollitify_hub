874a5fc510da505530ea0421256e5bfc9c3c4191
Dang Khoa <i.am@dangkhoa.dev>
2025-03-01 20:07:18 +0700

    update for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,51 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-if Rails.env.development?
-  user_params = {
-    email: 'admin@admin.admin',
+def get_email
+  return 'admin@admin.admin' if Rails.env.development?
+
+  if ENV['ADMIN_EMAIL'].present?
+    puts "Loaded admin email from environment variables"
+    ENV['ADMIN_EMAIL']
+  elsif Rails.application.credentials.admin_email.present?
+    puts "Loaded admin email from config/credentials.yml.enc"
+    Rails.application.credentials.admin_email
+  end
+end
+
+def get_password
+  return 'adminadmin' if Rails.env.development?
+
+  if ENV['ADMIN_PASSWORD'].present?
+    puts "Loaded admin password from environment variables"
+    ENV['ADMIN_PASSWORD']
+  elsif Rails.application.credentials.admin_password.present?
+    puts "Loaded admin password from config/credentials.yml.enc"
+    Rails.application.credentials.admin_password
+  end
+end
+
+email = get_email
+password = get_password
+
+if email && password
+  user = User.find_or_create_by(
+    email: email,
     username: 'admin',
     first_name: 'Admin',
     last_name: 'Master',
     role: 'admin'
-  }
-
-  user = User.find_by(user_params)
-  if user.present?
-    puts "Admin user already exists with email: #{user.email}"
-    return
-  end
-
-  user = User.new(user_params)
-  password = 'adminadmin'
+  )
 
   user.password = password
-  user.confirmed_at = Time.now
+  user.confirm
   user.save!
 
-  puts "Admin user created with email: #{user.email} and password: #{password}"
+  puts "Admin user created at #{Time.now} using provided credentials in environment variables or config/credentials.yml.enc."
+else
+  puts %Q(
+    Can't create admin user.
+    Please provide either the ADMIN_EMAIL environment variable or config/credentials.yml.enc with :admin_email,
+    and either the ADMIN_PASSWORD environment variable or config/credentials.yml.enc with :admin_password.
+  )
 end
