f6e062f5acf5bd6c881dcb31214e668efbbe728b
Scott Johnson <fuzzygroup@gmail.com>
2025-08-13 09:11:47 -0400

    Turn off db connection

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Rails8Authentication
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    
+    # Prevent connecting to DB during `assets:precompile`
+    config.assets.initialize_on_precompile = false
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
