c313b1997710b3cf81ed2e9cfaaad6d52c0195e9
Scott Johnson <fuzzygroup@gmail.com>
2025-08-15 07:45:51 -0400

    Disable action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,10 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+  #
+  # TURN OFF ACTION CABLE
+  #
+  config.action_cable.mount_path = nil
+  config.action_cable.allowed_request_origins = []
 end
