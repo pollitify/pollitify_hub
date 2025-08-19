60ed323df5225e90354177d7175f0a2259782072
Scott Johnson <fuzzygroup@gmail.com>
2025-08-19 05:31:41 -0400

    change migration for comments from feed_items to news_feed_items

--- db/migrate/20250817171128_create_comments.rb
+++ db/migrate/20250817171128_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[8.0]
   def change
     create_table :comments do |t|
       t.references :user, foreign_key: true
-      t.references :feed_item, foreign_key: true
+      t.references :news_feed_item, foreign_key: true
       t.text :body
 
       t.timestamps
