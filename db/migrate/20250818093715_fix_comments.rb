61ea4be42fb7f057a2504d1e2dd74bf6610fb801
Scott Johnson <fuzzygroup@gmail.com>
2025-08-19 05:47:02 -0400

    remove bad migration

--- db/migrate/20250818093715_fix_comments.rb
+++
@@ -1,6 +0,0 @@
-class FixComments < ActiveRecord::Migration[8.0]
-  def change
-    add_reference :comments, :news_feed_item, null: false, foreign_key: true
-    remove_column :comments, :feed_item_id
-  end
-end
